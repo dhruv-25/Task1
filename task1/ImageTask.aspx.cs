f564eee8c19983512ef5f80de296bab677a64dbc
Dhruv Suthar <dhruvsuthar025@hotmail.com>
2019-09-05 12:11:56 +0530

    Add project files.

---
+++ task1/ImageTask.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task1
+{
+    public partial class ImageTask : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(Server.MapPath("Images")))
+            {
+                Directory.CreateDirectory(Server.MapPath("Images"));
+            }
+            string imageurl = (Server.MapPath("Images") + "\\" + fileupload.FileName);
+            fileupload.SaveAs(imageurl);
+        }
+    }
+}
